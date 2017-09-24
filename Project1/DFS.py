74b50fcf262c9513ed8b4559063e9d39b8fac801
WenzhongLi <wenzhongli7@gmail.com>
2017-09-23 20:18:31 -0400

    add optimal road discover

--- Project1/DFS.py
+++ Project1/DFS.py
@@ -3,14 +3,19 @@
 '''
 @author: li
 '''
+import copy
 import sys
 import Start
+
+
 class DFS(object):
     # 初始化迷宫大小和密度
     def __init__(self):
         self.node_to_go = {}
         self.node_has_been = dict()
         self.size = -1
+        self.min_distance = -1
+        self.optimal_road = []
         #self.density = density
         #self.map_matrix = []
         #for i in range(size):
@@ -24,9 +29,13 @@ class DFS(object):
         self.node_to_go = {}
         self.node_has_been = dict()
         self.size = size
+        self.min_distance = size * size
         start_node = (0, 0)
         end_node = (size - 1, size - 1)
         self.dfs_step(map, start_node, end_node, 0, [])
+        if self.optimal_road:
+            print "optimal_road",
+            print self.optimal_road
 
     def dfs_step(self, map, current_node, end_node, distance_traveled, tracert):
         if current_node[0] < 0 or current_node[1] < 0 or current_node[1] \
@@ -37,22 +46,37 @@ class DFS(object):
         distance_traveled+=1
         tracert.append(current_node)
         if current_node == end_node:
+            #rint "success"
             print tracert
-            print "success"
+            if len(tracert) < self.min_distance:
+                self.min_distance = len(tracert)
+                self.optimal_road = copy.deepcopy(tracert)
             return
         #print current_node
-        self.node_has_been[current_node] = 1
+        self.node_has_been[current_node] = len(tracert)
+        backup_tracert = copy.deepcopy(tracert)
         # +(1, 0)
         if (current_node[0]+1, current_node[1]) not in self.node_has_been:
             self.dfs_step(map, (current_node[0] + 1, current_node[1]), end_node, distance_traveled, tracert)
+        elif self.node_has_been[(current_node[0]+1, current_node[1])] > (len(tracert) + 1):
+            self.dfs_step(map, (current_node[0] + 1, current_node[1]), end_node, distance_traveled, tracert)
+        tracert = copy.deepcopy(backup_tracert)
         # +(0, 1)
         if (current_node[0], current_node[1]+1) not in self.node_has_been:
             self.dfs_step(map, (current_node[0], current_node[1] + 1), end_node, distance_traveled, tracert)
+        elif self.node_has_been[(current_node[0], current_node[1] + 1)] > (len(tracert) + 1):
+            self.dfs_step(map, (current_node[0], current_node[1] + 1), end_node, distance_traveled, tracert)
+        tracert = copy.deepcopy(backup_tracert)
         # +(-1, 0)
         if (current_node[0] - 1, current_node[1]) not in self.node_has_been:
             self.dfs_step(map, (current_node[0] - 1, current_node[1]), end_node, distance_traveled, tracert)
+        elif self.node_has_been[(current_node[0] - 1, current_node[1])] > (len(tracert) + 1):
+            self.dfs_step(map, (current_node[0] - 1, current_node[1]), end_node, distance_traveled, tracert)
+        tracert = copy.deepcopy(backup_tracert)
         # +(0, -1)
         if (current_node[0], current_node[1] - 1) not in self.node_has_been:
+            self.dfs_step(map, (current_node[0], current_node[1] - 1), end_node, distance_traveled, tracert)
+        elif self.node_has_been[(current_node[0], current_node[1] - 1)] > (len(tracert) + 1):
             self.dfs_step(map, (current_node[0], current_node[1] - 1), end_node, distance_traveled, tracert)
         return
 
