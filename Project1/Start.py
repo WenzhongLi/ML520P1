2a2072e45cef599ad815e65ce5cf149762777bbf
WenzhongLi <wenzhongli7@gmail.com>
2017-09-25 12:11:59 -0400

    new random

--- Project1/Start.py
+++ Project1/Start.py
@@ -4,6 +4,7 @@
 import random
 import sys
 
+
 class Start(object):
     # 初始化迷宫大小和密度
     def __init__(self,size,density):
@@ -14,7 +15,8 @@ class Start(object):
             self.map_matrix.append([])
             for j in range(size):
                 self.map_matrix[i].append(0)
-    #输出当前迷宫到控制台
+    # 输出当前迷宫到控制台
+
     def print_matrix(self):
         count_blocked = 0
         for i in range(self.size):
@@ -24,45 +26,55 @@ class Start(object):
                     count_blocked+=1
             print('\n'),
         print(str(count_blocked)+" are blocked\n")
-    #随机绘制迷宫
+    # 随机绘制迷宫
+
     def paint_random(self):
-        i=0
-        total_num = self.size*self.size - 2
-        block_num = int(self.size*self.size*self.density)
-        while(i<block_num):
-            target_num = random.randint(1, total_num - i)
-            #self.print_matrix()
-            #print (i,target_num,total_num +1 - i)
-            count = 0
-            finish_flag = 0
+        list = []
+        for i in range(self.size):
             for j in range(self.size):
-                for k in range(self.size):
-                    if count == target_num:
-                        if self.map_matrix[j][k] == 1:
-                            continue
-                        elif self.map_matrix[j][k] == 0:
-                            self.map_matrix[j][k] = 1
-                            finish_flag = 1
-                            break
-                        else:#
-                            print 'error'
-                    if self.map_matrix[j][k] == 0:
-                        count+=1
-                if finish_flag == 1:
-                    break
-            i+=1
-            finish_flag = 0
-        random.uniform(10, 20)
-    #返回迷宫矩阵
+                list.append((i, j))
+        slice = random.sample(list, int(self.size*self.size*self.density))
+        for node in slice:
+            self.map_matrix[node[0]][node[1]] = 1
+        # i=0
+        # total_num = self.size*self.size - 2
+        # block_num = int(self.size*self.size*self.density)
+        # while(i<block_num):
+        #     target_num = random.randint(1, total_num - i)
+        #     #self.print_matrix()
+        #     #print (i,target_num,total_num +1 - i)
+        #     count = 0
+        #     finish_flag = 0
+        #     for j in range(self.size):
+        #         for k in range(self.size):
+        #             if count == target_num:
+        #                 if self.map_matrix[j][k] == 1:
+        #                     continue
+        #                 elif self.map_matrix[j][k] == 0:
+        #                     self.map_matrix[j][k] = 1
+        #                     finish_flag = 1
+        #                     break
+        #                 else:#
+        #                     print 'error'
+        #             if self.map_matrix[j][k] == 0:
+        #                 count+=1
+        #         if finish_flag == 1:
+        #             break
+        #     i+=1
+        #     finish_flag = 0
+        # random.uniform(10, 20)
+    # 返回迷宫矩阵
+
     def get_matrix(self):
         return self.map_matrix
+
 
 if __name__ == "__main__":
     print "script_name", sys.argv[0]
     for i in range(1, len(sys.argv)):
         print "argment", i, sys.argv[i]
     print ('start initialize')
-    #set the size and density of this matrix
+    # set the size and density of this matrix
     start = Start(10,0.3)
     start.print_matrix()
     start.paint_random()
