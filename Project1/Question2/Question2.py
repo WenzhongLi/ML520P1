8921132239c830d0332fd8416dc7dc121e822a07
WenzhongLi <wenzhongli7@gmail.com>
2017-09-29 15:34:09 -0400

    save data for vision

--- Project1/Question2/Question2.py
+++ Project1/Question2/Question2.py
@@ -11,12 +11,16 @@ sys.path.append("..")
 import Start
 import BFS
 import DFS
+import ASTAR_MHT
+import ASTAR_EUC
 
 if __name__ == "__main__":
     print "script_name", sys.argv[0]
     for i in range(1, len(sys.argv)):
         print "argument", i, sys.argv[i]
     print ('start initialize')
+    f1 = open('data.js', 'w')
+    # f1.write('hello boy!')
     dfs = DFS.DFS()
     size = 10
     start = Start.Start(size, 0.3)
@@ -28,10 +32,9 @@ if __name__ == "__main__":
         start.paint_random()
     map = copy.copy(start.get_matrix())
     data = dict()
-    original = {"size": size, "map": map}#[[1,1,1],[1,1,1],[1,1,1]]}
+    original = {"size": size, "map": map}# [[1,1,1],[1,1,1],[1,1,1]]}
     data["original"] = original
     # DFS
-
     result = dfs.dfs_route(start.get_matrix(), size)
     route = dfs.print_optimal(copy.deepcopy(map))
     # print result
@@ -41,40 +44,31 @@ if __name__ == "__main__":
     result = bfs.bfs_init(start.get_matrix(), size)
     route = copy.deepcopy(map)
     for node in result[1]:
-
+        route[node[0]][node[1]] = 2
+    # for i in range(0,size):
+    #     for j in range(0, size):
+    #         print route[i][j],
+    #     print '\n',
+    data["BFS"] = {"size": size, "map": route}
     print result
     # A-MHT
+    astar = ASTAR_MHT.ASTAR()
+    result = astar.find_path(start.get_matrix(), size)
+    route = copy.deepcopy(map)
+    for node in result[1]:
+        route[node[0]][node[1]] = 2
+    data["MHT"] = {"size": size, "map": route}
+    print result
     # A-Eud
-
-
+    astar = ASTAR_EUC.ASTAR()
+    result = astar.find_path(start.get_matrix(), size)
+    route = copy.deepcopy(map)
+    for node in result[1]:
+        route[node[0]][node[1]] = 2
+    data["EUC"] = {"size": size, "map": route}
+    print result
     json = json.dumps(data)
-    print json
-
-
-    size = 100
-    # start.print_matrix()
-    # start.paint_random()
-    # start.print_matrix()
-    print ('start over')
-    bfs = BFS.BFS()
-    # dfs.dfs_route(start.get_matrix(), size)
-    print "density, percentage"
-    for i in range(0, 450):
-        density = float(i)/1000
-        # print "density: ",
-        # print density
-
-        count = 0;
-        success_3_count = 0;
-        start = Start.Start(size, density)
-        while count < 100:
-            start.paint_random()
-            success = bfs.bfs_init(start.get_matrix(), size)
-            if success[0] == 1:
-                count += 1
-                success_3_count += success[2]
-        # print "percentage: ",
-        print density,
-        print float(success_3_count)/100
-    # dfs.dfs_route(m, size)
-    print ('end')
+    f1.write("var json_data = " + json + ";")
+    f1.flush()
+    f1.close()
+    print "end"
